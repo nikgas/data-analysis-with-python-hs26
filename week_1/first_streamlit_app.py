053b69a2163025b9478ac250d593e349bb07e57f
Niklas Gasser <niklas.gasser@bluewin.ch>
2026-09-18 16:26:23 +0200

    rock paper scissors works now

--- week_1/first_streamlit_app.py
+++ week_1/first_streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import random
 
 st.title("Niklas's first app")
 
@@ -6,11 +7,21 @@ with st.chat_message("assistant"):
     st.markdown("Hello, I have a square head. Let's play rock paper scissors.")
 with st.chat_message("user"):
     user_choice = st.chat_input("Type your choice, you may use the whole word or only the letters r, p, s.")
-allowed_choices = ["rock", "paper", "scissors", "r", "p", "s"]
+allowed_choices = ["r", "p", "s"]
+if user_choice:
+    user_choice = user_choice[0].lower()
+
 if user_choice in allowed_choices:
-    assistant_choice = choice(["rock", "paper", "scissors"])
-    with st.chat_message("assistant"):
-        st.markdown(f"You chose: {user_choice}")
+    assistant_choice = random.choice(allowed_choices)
+    if assistant_choice == user_choice:   
+        with st.chat_message("assistant"):
+            st.markdown(f"It's a draw, I chose {assistant_choice} too. Make a new choice to play again.")
+    elif (assistant_choice == "r" and user_choice == "s") or (assistant_choice == "p" and user_choice == "r") or (assistant_choice == "s" and user_choice == "p"):
+        with st.chat_message("assistant"):
+            st.markdown(f"I win! {assistant_choice} beats {user_choice}. Make a new choice to play again.")
+    elif (assistant_choice == "r" and user_choice == "p") or (assistant_choice == "p" and user_choice == "s") or (assistant_choice == "s" and user_choice == "r"):
+        with st.chat_message("assistant"):
+            st.markdown(f"You win! {user_choice} beats {assistant_choice}. Make a new choice to play again.")
 else:
     with st.chat_message("assistant"):
         st.markdown("You did not choose a valid option. Please try again.")
