8b9906d7e14e2819d72e5f8a0e5ce275abedadbf
Niklas Gasser <niklas.gasser@bluewin.ch>
2026-09-23 15:46:58 +0200

    pdf to png now works on real folder structure and is stable

--- week_2/pdf_to_png.py
+++ week_2/pdf_to_png.py
@@ -3,11 +3,11 @@ from pathlib import Path
 
 folder = Path("C:/Users/floba/OneDrive - Universität Zürich UZH/HS26/data_analysis_with_python/data-analysis-with-python-hs26/week_2/pdf_folder")
 
-for pdf in folder.glob("*.pdf"):
-    pdf_folder = folder.parent / pdf.stem
-    pdf_folder.mkdir()
-    pdf_file = pymupdf.open(pdf)
-    for i, page in enumerate(pdf_file):
-       pix = page.get_pixmap(dpi=300)
-       pix.save(f"{pdf_folder}/page-{i}.png")
-pdf_file.close()
+for pdf in folder.rglob("*.pdf"):
+    png_folder = pdf.parent / f"{pdf.stem}_png"
+    if not png_folder.exists():
+        png_folder.mkdir()
+        with pymupdf.open(pdf) as pdf_file:
+            for i, page in enumerate(pdf_file):
+                pix = page.get_pixmap(dpi=300)
+                pix.save(f"{png_folder}/page-{i}.png")
