b0b3dfb4cf45b3d528a09078b8c540d0fe0d02bd
Niklas Gasser <niklas.gasser@bluewin.ch>
2026-09-23 22:25:01 +0200

    first time working with classes: seems very powerful (if not only used for one function, like i did). code has currently an error: there seems something wrong with the pdf-path in the handler. also: implement functionality that already existing pdfs in folder are converted plus implement a possibility to conveniently stop the observer.

--- week_2/pdf_to_png.py
+++ week_2/pdf_to_png.py
@@ -1,13 +1,30 @@
 import pymupdf
 from pathlib import Path
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
 folder = Path("C:/Users/floba/OneDrive - Universität Zürich UZH/HS26/data_analysis_with_python/data-analysis-with-python-hs26/week_2/pdf_folder")
 
-for pdf in folder.rglob("*.pdf"):
+def pdf_to_png(pdf):
     png_folder = pdf.parent / f"{pdf.stem}_png"
     if not png_folder.exists():
         png_folder.mkdir()
-        with pymupdf.open(pdf) as pdf_file:
-            for i, page in enumerate(pdf_file):
-                pix = page.get_pixmap(dpi=300)
-                pix.save(f"{png_folder}/page-{i}.png")
+    with pymupdf.open(pdf) as pdf_file:
+        for i, page in enumerate(pdf_file):
+            pix = page.get_pixmap(dpi=300)
+            pix.save(f"{png_folder}/page-{i}.png")
+
+class MyHandler(FileSystemEventHandler):
+    def on_any_event(self, event):
+        if not event.is_directory:
+            if event.event_type in ("created", "modified"):
+                pdf = Path(event.src_path)
+                if pdf.suffix.lower() == ".pdf":
+                    pdf_to_png(pdf)
+
+handler = MyHandler()
+
+observer = Observer()
+observer.schedule(handler, folder, recursive=True)
+observer.start()
+observer.join()
